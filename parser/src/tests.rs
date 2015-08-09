ac64e5e2898baad007b8c40dafd7f2e3d68c5665
Hawk Weisman <hi@hawkweisman.me>
2015-08-09 18:15:08 -0400

    Parser test suite now works with new parser

--- parser/src/tests.rs
+++ parser/src/tests.rs
@@ -2,30 +2,29 @@ use ::ast::*;
 use ::ast::ExprNode::*;
 use ::ast::NumNode::*;
 
-use super::*;
-use super::combine::*;
-use super::combine_language::{LanguageEnv, LanguageDef, Identifier};
+use super::parse;
 
 macro_rules! test_parse {
     ($name:ident, $string:expr,$expect:expr) => {
-        #[test] fn $name () {assert_eq!(parse($string), Ok($expect)) }
+        #[test] #[allow(non_snake_case)] fn $name () {
+            assert_eq!(parse($string), Ok($expect)) }
     };
 }
 
-test_parse!(test_ident,
+test_parse!(ident,
     "ident", Name(NameNode { name: "ident".to_string() })
 );
 
-test_parse!(test_ident_single_char,
+test_parse!(ident_single_char,
     "a", Name(NameNode { name: "a".to_string() })
 );
 
-test_parse!(test_ident_special_chars,
+test_parse!(ident_special_chars,
     "ident_With\\special!Chars:~-+",
     Name(NameNode { name: "ident_With\\special!Chars:~-+".to_string() })
 );
 
-test_parse!(test_sexpr,
+test_parse!(sexpr,
     "(ident arg1 arg2)",
     SExpr(SExprNode {
         operator: Box::new(Name(NameNode { name: "ident".to_string() })),
@@ -36,7 +35,7 @@ test_parse!(test_sexpr,
     })
 );
 
-test_parse!(test_sexpr_square_bracket,
+test_parse!(sexpr_square_bracket,
     "[ident arg1 arg2]",
     SExpr(SExprNode {
         operator: Box::new(Name(NameNode { name: "ident".to_string() })),
@@ -48,18 +47,177 @@ test_parse!(test_sexpr_square_bracket,
 );
 
 // signed integers
-test_parse!(test_sint_positive, "1234", NumConst(IntConst(IntNode { value: 1234 }) ));
-test_parse!(test_sint_positive_d, "#d1234", NumConst(IntConst(IntNode { value: 1234 }) ));
-test_parse!(test_sint_positive_D, "#D1234", NumConst(IntConst(IntNode { value: 1234 }) ));
-test_parse!(test_sint_negative, "-1234", NumConst(IntConst(IntNode { value: -1234 }) ));
-test_parse!(test_sint_hex_x, "#x0ff", NumConst(IntConst(IntNode { value: 0x0ff }) ));
-test_parse!(test_sint_hex_X, "#X0FF", NumConst(IntConst(IntNode { value: 0x0ff }) ));
+test_parse!(sint_positive, "1234", NumConst(IntConst(IntNode { value: 1234 }) ));
+test_parse!(sint_positive_d, "#d1234", NumConst(IntConst(IntNode { value: 1234 }) ));
+test_parse!(sint_positive_D, "#D1234", NumConst(IntConst(IntNode { value: 1234 }) ));
+test_parse!(sint_negative, "-1234", NumConst(IntConst(IntNode { value: -1234 }) ));
+test_parse!(sint_hex_x, "#x0ff", NumConst(IntConst(IntNode { value: 0x0ff }) ));
+test_parse!(sint_hex_X, "#X0FF", NumConst(IntConst(IntNode { value: 0x0ff }) ));
 
 // unsigned integers
-test_parse!(test_uint_u, "1234u", NumConst(UIntConst(UIntNode{ value: 1234 })));
-test_parse!(test_uint_U, "1234U", NumConst(UIntConst(UIntNode{ value: 1234 })));
-test_parse!(test_uint_hex_x, "#x0ffu", NumConst(UIntConst(UIntNode{ value: 0x0ff })));
-test_parse!(test_uint_hex_X, "#X0FFU", NumConst(UIntConst(UIntNode{ value: 0x0ff })));
+test_parse!(uint_u, "1234u", NumConst(UIntConst(UIntNode{ value: 1234 })));
+test_parse!(uint_U, "1234U", NumConst(UIntConst(UIntNode{ value: 1234 })));
+test_parse!(uint_hex_x, "#x0ffu", NumConst(UIntConst(UIntNode{ value: 0x0ff })));
+test_parse!(uint_hex_X, "#X0FFU", NumConst(UIntConst(UIntNode{ value: 0x0ff })));
+
+// floats
+test_parse!(float_decimal_1, "1.0", NumConst(FloatConst(FloatNode { value: 1.0f64 })));
+// NYI
+// test_parse!(float_1f, "1f", NumConst(FloatConst(FloatNode { value: 1.0f64 })));
+// test_parse!(float_1F, "1F", NumConst(FloatConst(FloatNode { value: 1.0f64 })));
+test_parse!(float_decimal_repeating,
+    "22.2", NumConst(FloatConst(FloatNode { value: 22.2 }))
+);
+// NYI
+// test_parse!(float_decimal_repeating_f,
+//     "22.2222f", NumConst(FloatConst(FloatNode { value: 22.2222 }))
+// );
+// test_parse!(float_decimal_repeating_F,
+//     "22.2222F", NumConst(FloatConst(FloatNode { value: 22.2222 }))
+// );
+
+/// This is the parsing component of basic arithmetic
+/// integration target
+///
+/// ```lisp
+/// (+ 10 10)
+/// ```
+test_parse!(basic_arith,
+    "(+ 10 10)",
+    SExpr(SExprNode {
+        operator: box Name(NameNode { name: "+".to_string() }),
+        operands: vec![
+            NumConst(IntConst(IntNode{ value: 10 })),
+            NumConst(IntConst(IntNode{ value: 10 }))
+        ]
+    })
+);
+
+
+/// This is the parsing component of the CAR integration target
+///
+/// ```lisp
+/// (car (cons 10 (cons 20 nil)))
+/// ```
+test_parse!(list_car, "(car (cons 10 (cons 20 nil)))",
+    SExpr(SExprNode {
+        operator: box Name(NameNode { name: "car".to_string() }),
+        operands: vec![
+            SExpr(SExprNode {
+                operator: box Name(NameNode { name: "cons".to_string() }),
+                operands: vec![
+                    NumConst(IntConst(IntNode{ value: 10 })),
+                    SExpr(SExprNode {
+                        operator: box Name(NameNode { name: "cons".to_string() }),
+                        operands: vec![
+                            NumConst(IntConst(IntNode{ value: 20 })),
+                            Name(NameNode { name: "nil".to_string() })
+                        ]
+                    })
+                ]
+            })
+        ]
+    })
+);
+
+/// This is the parsing component of the CDR integration target
+///
+/// ```lisp
+/// (cdr (cons 10 (cons 20 nil)))
+/// ```
+test_parse!(list_cdr, "(cdr (cons 10 (cons 20 nil)))",
+    SExpr(SExprNode {
+        operator: box Name(NameNode { name: "cdr".to_string() }),
+        operands: vec![
+            SExpr(SExprNode {
+                operator: box Name(NameNode { name: "cons".to_string() }),
+                operands: vec![
+                    NumConst(IntConst(IntNode{ value: 10 })),
+                    SExpr(SExprNode {
+                        operator: box Name(NameNode { name: "cons".to_string() }),
+                        operands: vec![
+                            NumConst(IntConst(IntNode{ value: 20 })),
+                            Name(NameNode { name: "nil".to_string() })
+                        ]
+                    })
+                ]
+            })
+        ]
+    })
+);
+/// This is the parsing component of nested arithmetic
+/// integration target.
+///
+/// ```lisp
+/// (- 20 (+ 5 5))
+/// ```
+test_parse!(nested_arith, "(- 20 (+ 5 5))",
+    SExpr(SExprNode {
+        operator: box Name(NameNode { name: "-".to_string() }),
+        operands: vec![
+            NumConst(IntConst(IntNode{ value: 20 })),
+            SExpr(SExprNode {
+                operator: box Name(NameNode { name: "+".to_string() }),
+                operands: vec![
+                    NumConst(IntConst(IntNode{ value: 5 })),
+                    NumConst(IntConst(IntNode{ value: 5 }))
+                ]
+            })
+        ]
+    })
+);
+/// This is the parsing component of nested arithmetic
+/// integration target with square brackets added
+/// (just to ensure that the parser handles them correctly)
+///
+/// ```lisp
+/// (- 20 [+ 5 5])
+/// ```
+test_parse!(nested_arith_square_bracket, "(- 20 [+ 5 5])",
+    SExpr(SExprNode {
+        operator: box Name(NameNode { name: "-".to_string() }),
+        operands: vec![
+            NumConst(IntConst(IntNode{ value: 20 })),
+            SExpr(SExprNode {
+                operator: box Name(NameNode { name: "+".to_string() }),
+                operands: vec![
+                    NumConst(IntConst(IntNode{ value: 5 })),
+                    NumConst(IntConst(IntNode{ value: 5 }))
+                ]
+            })
+        ]
+    })
+);
+
+
+/// This is the parsing component of the `compile_basic_branching_1`
+/// integration target.
+///
+/// ```lisp
+/// (if (= 0 (- 1 1)) #t #f)
+/// ```
+test_parse!(basic_branching_1, "(if (= 0 (- 1 1)) #t #f)",
+    SExpr(SExprNode {
+        operator: box Name(NameNode::new("if".to_string())),
+        operands: vec![
+            SExpr(SExprNode{
+                operator: box Name(NameNode::new("=".to_string())),
+                operands: vec![
+                    NumConst(IntConst(IntNode{value: 0})),
+                    SExpr(SExprNode{
+                        operator: box Name(NameNode::new("-".to_string())),
+                        operands: vec![
+                            NumConst(IntConst(IntNode{ value: 1 })),
+                            NumConst(IntConst(IntNode{ value: 1 }))
+                        ]
+                    })
+                ]
+            }),
+            BoolConst(BoolNode{value:true}),
+            BoolConst(BoolNode{value:false}),
+        ]
+    })
+ );
 
 // /* // Currently unsupported
 // #[test]
@@ -73,208 +231,8 @@ test_parse!(test_uint_hex_X, "#X0FFU", NumConst(UIntConst(UIntNode{ value: 0x0ff
 //         Ok((IntConst(IntNode { value: 0b01isize }), ""))
 //         );
 // }*/
+//
 
-// #[test]
-// fn test_lex_float() {
-//     assert_eq!(
-//         parser(number).parse("1.0"),
-//         Ok((FloatConst(FloatNode { value: 1.0f64 }), ""))
-//         );/* // Unsupported
-//     assert_eq!(
-//         parser(number).parse("1f").unwrap(),
-//         (FloatConst(FloatNode { value: 1.0f64 }), "")
-//         );
-//     assert_eq!(
-//         parser(number).parse("22.2222").unwrap(),
-//         (FloatConst(FloatNode { value: 22.2222f64 }), "")
-//         );
-//     assert_eq!(
-//         parser(number).parse("22.2222f").unwrap(),
-//         (FloatConst(FloatNode { value: 22.2222f64 }), "")
-//         );*/
-// }
-//
-// /// This is the parsing component of basic arithmetic
-// /// integration target
-// ///
-// /// ```lisp
-// /// (+ 10 10)
-// /// ```
-// #[test]
-// fn test_parse_arith() {
-//     assert_eq!(
-//         parser(expr).parse("(+ 10 10)"),
-//         Ok((
-//             SExpr(SExprNode {
-//                 operator: box Name(NameNode { name: "+".to_string() }),
-//                 operands: vec![
-//                     NumConst(IntConst(IntNode{ value: 10 })),
-//                     NumConst(IntConst(IntNode{ value: 10 }))
-//                 ]
-//             }),
-//             ""))
-//         );
-// }
-//
-// /// This is the parsing component of the CAR integration target
-// ///
-// /// ```lisp
-// /// (car (cons 10 (cons 20 nil)))
-// /// ```
-// #[test]
-// fn test_parse_car() {
-//     assert_eq!(
-//         parser(expr).parse("(car (cons 10 (cons 20 nil)))"),
-//         Ok((
-//             SExpr(SExprNode {
-//                 operator: box Name(NameNode { name: "car".to_string() }),
-//                 operands: vec![
-//                     SExpr(SExprNode {
-//                         operator: box Name(NameNode { name: "cons".to_string() }),
-//                         operands: vec![
-//                             NumConst(IntConst(IntNode{ value: 10 })),
-//                             SExpr(SExprNode {
-//                                 operator: box Name(NameNode { name: "cons".to_string() }),
-//                                 operands: vec![
-//                                     NumConst(IntConst(IntNode{ value: 20 })),
-//                                     Name(NameNode { name: "nil".to_string() })
-//                                 ]
-//                             })
-//                         ]
-//                     })
-//                 ]
-//             }),
-//             ""))
-//         );
-// }
-//
-//
-// /// This is the parsing component of the CDR integration target
-// ///
-// /// ```lisp
-// /// (cdr (cons 10 (cons 20 nil)))
-// /// ```
-// #[test]
-// fn test_parse_cdr() {
-//     assert_eq!(
-//         parser(expr).parse("(cdr (cons 10 (cons 20 nil)))"),
-//         Ok((
-//             SExpr(SExprNode {
-//                 operator: box Name(NameNode { name: "cdr".to_string() }),
-//                 operands: vec![
-//                     SExpr(SExprNode {
-//                         operator: box Name(NameNode { name: "cons".to_string() }),
-//                         operands: vec![
-//                             NumConst(IntConst(IntNode{ value: 10 })),
-//                             SExpr(SExprNode {
-//                                 operator: box Name(NameNode { name: "cons".to_string() }),
-//                                 operands: vec![
-//                                     NumConst(IntConst(IntNode{ value: 20 })),
-//                                     Name(NameNode { name: "nil".to_string() })
-//                                 ]
-//                             })
-//                         ]
-//                     })
-//                 ]
-//             }),
-//             ""))
-//         );
-// }
-//
-// /// This is the parsing component of nested arithmetic
-// /// integration target with square brackets added
-// /// (just to ensure that the parser handles them correctly)
-// ///
-// /// ```lisp
-// /// (- 20 [+ 5 5])
-// /// ```
-// #[test]
-// fn test_parse_nested_arith_square_bracket() {
-//     assert_eq!(
-//         parser(expr).parse("(- 20 [+ 5 5])"),
-//         Ok((
-//             SExpr(SExprNode {
-//                 operator: box Name(NameNode { name: "-".to_string() }),
-//                 operands: vec![
-//                     NumConst(IntConst(IntNode{ value: 20 })),
-//                     SExpr(SExprNode {
-//                         operator: box Name(NameNode { name: "+".to_string() }),
-//                         operands: vec![
-//                             NumConst(IntConst(IntNode{ value: 5 })),
-//                             NumConst(IntConst(IntNode{ value: 5 }))
-//                         ]
-//                     })
-//                 ]
-//             }),
-//             ""))
-//         );
-// }
-//
-// /// This is the parsing component of nested arithmetic
-// /// integration target
-// ///
-// /// ```lisp
-// /// (- 20 (+ 5 5))
-// /// ```
-// #[test]
-// fn test_parse_nested_arith() {
-//     assert_eq!(
-//         parser(expr).parse("(- 20 (+ 5 5))"),
-//         Ok((
-//             SExpr(SExprNode {
-//                 operator: box Name(NameNode { name: "-".to_string() }),
-//                 operands: vec![
-//                     NumConst(IntConst(IntNode{ value: 20 })),
-//                     SExpr(SExprNode {
-//                         operator: box Name(NameNode { name: "+".to_string() }),
-//                         operands: vec![
-//                             NumConst(IntConst(IntNode{ value: 5 })),
-//                             NumConst(IntConst(IntNode{ value: 5 }))
-//                         ]
-//                     })
-//                 ]
-//             }),
-//             ""))
-//         );
-// }
-//
-// /// This is the parsing component of the `compile_basic_branching_1`
-// /// integration target.
-// ///
-// /// ```lisp
-// /// (if (= 0 (- 1 1)) #t #f)
-// /// ``
-// #[test]
-// fn test_parse_basic_branching_1() {
-//     assert_eq!(
-//         parser(expr).parse("(if (= 0 (- 1 1)) #t #f)"),
-//         Ok((
-//             SExpr(SExprNode {
-//                 operator: box Name(NameNode::new("if".to_string())),
-//                 operands: vec![
-//                     SExpr(SExprNode{
-//                         operator: box Name(NameNode::new("=".to_string())),
-//                         operands: vec![
-//                             NumConst(IntConst(IntNode{value: 0})),
-//                             SExpr(SExprNode{
-//                                 operator: box Name(NameNode::new("-".to_string())),
-//                                 operands: vec![
-//                                     NumConst(IntConst(IntNode{ value: 1 })),
-//                                     NumConst(IntConst(IntNode{ value: 1 }))
-//                                 ]
-//                             })
-//                         ]
-//                     }),
-//                     BoolConst(BoolNode{value:true}),
-//                     BoolConst(BoolNode{value:false}),
-//                 ]
-//             }
-//             ),
-//             "")
-//         )
-//     )
-// }
-//
 // /// This is the parsing component of the `compile_basic_branching_2`
 // /// integration target.
 // ///
